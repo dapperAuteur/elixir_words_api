7160495bca0132fcf46d5127d613fcf0c8c57990
Anthony McDonald <a@awews.com>
2017-10-09 16:55:34 -0600

    updates for heroku deployment

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ElixirWordsApi.UserSocket do
   # channel "room:*", ElixirWordsApi.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
